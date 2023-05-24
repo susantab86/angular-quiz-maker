df8ecb42dffe694697712c0292c48c76875e6c1e
susantab86 <130149121+susantab86@users.noreply.github.com>
2023-05-25 03:29:56 +0530

    2 files modified

--- src/app/Shared/quizmaker.modal.ts
+++ src/app/Shared/quizmaker.modal.ts
@@ -1,12 +1,3 @@
-export interface TeamList {
-  id: number;
-  abbreviation: string;
-  city: string;
-  conference: string;
-  division: string;
-  full_name: string;
-  name: string;
-}
 export interface difficulty {
   name: string;
   code: string;
@@ -26,53 +17,13 @@ export interface category {
 export interface quizCategory {
   data: Array<category>;
 }
-export interface TeamListDeatils {
-  id: number;
-  abbreviation: string;
-  city: string;
-  conference: string;
-  division: string;
-  full_name: string;
-  name: string;
-  selfAvgScore: number;
-  opptAvgScore: number;
-  results: Array<any>;
-}
 
-export interface teamdetails {
-  team: TeamList;
-  score: Array<any>;
-}
-
-export interface teamResults {
-  id: number;
-  date: string;
-  home_team_score: number;
-  visitor_team_score: number;
-  period: number;
-  postseason: boolean;
-  season: number;
-  status: string;
-  time: string;
-  visitor_team: home_team;
-  home_team: home_team;
-}
-
-export interface home_team {
-  id: number;
-  abbreviation: string;
-  city: string;
-  conference: string;
-  division: string;
-  full_name: string;
-  name: string;
-}
 export interface question {
-  category:string;
-  correct_answer:string;
-  difficulty:string;
-  incorrect_answers:Array<string>;
-  question:string;
-  type:string;
-  selectedAnsw:string;
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: Array<string>;
+  question: string;
+  type: string;
+  selectedAnsw: string;
 }
